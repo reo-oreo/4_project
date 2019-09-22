866515f84030ee0efc6b6013a7a85999d1b8b2e7
小野澤礼央 <onozawareo@onozawareo-no-MacBook-Air.local>
2019-09-22 09:58:57 +0900

    finished solution1

--- kaggle/titanic/solution1.py
+++ kaggle/titanic/solution1.py
@@ -1,28 +1,27 @@
-#データ分析と処理
+#ライブラリ
+
+    #データ分析と処理系ライブラリ
 import pandas as pd
 import numpy as np
 import random as rnd
 
 
-#ビジュアル化
+    #ビジュアル化ライブラリ
 import seaborn as sns
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-#機械学習
+    #機械学習ライブラリ
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC,LinearSVC
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.linear_model import Perceptron
-from sklearn.linear_model import Perceptron
 from sklearn.linear_model import SGDClassifier
 from sklearn.tree import    DecisionTreeClassifier
 
-
-#データの取得
-
+#データ取得
 train_df=pd.read_csv("./train.csv")
 test_df=pd.read_csv("./test.csv")
 combine=[train_df,test_df]
@@ -30,36 +29,36 @@ combine=[train_df,test_df]
 # sns.pairplot(train_df)
 # plt.show()
 
-#可視化によるデータの分析
+#データ確認
 
-#列名を表示させる
+
+    #列名を表示させる
 print(train_df.columns.values)
 
-#データのプレビュー
+    #データのプレビュー
 print(train_df.head())
 
-#infoを使って、欠損値をもつデータ項目と変数の種類を見分ける
+    #infoを使って、欠損値をもつデータ項目と変数の種類を見分ける
 print("■注意：418に満たない列名は欠損値を含まない、またデータ型がint,floatでない場合は質的変数とわかる")
 train_df.info()
 print('_'*40)
 test_df.info()
 
 print("---------")
-#基本統計量の確認
+    #基本統計量の確認
 print("■基本統計量をここで確認する")
 print(train_df.describe())
 
-#質的変数の確認
+    #質的変数の確認
 print("■質的変数の分布をここで確認する")
 print(train_df.describe(include='O'))
 print("-----"*10)
 print(train_df.describe(include=np.number))
 
-#データ分析に基づく推測
-#相関、終了、修正、作成、分類
 
 
-#特徴をピボットすることで分析
+
+    #２変数の関係性
 
 print("■PclassとSurvivedのピボット")
 print(train_df[['Pclass','Survived']].groupby(['Pclass'],as_index=False).mean().sort_values(by='Survived',ascending=False))
@@ -73,36 +72,33 @@ print(train_df[['SibSp','Survived']].groupby(['SibSp'],as_index=False).mean().so
 print("■ParchとSurvivedのピボット")
 print(train_df[['Parch','Survived']].groupby(['Parch'],as_index=False).mean().sort_values(by='Survived',ascending=False))
 
-#可視化によるデータ分析
 
-
-#量的変数同士の相関
+    #量的変数同士の相関
 g=sns.FacetGrid(train_df,col='Survived')
 g.map(plt.hist,'Age',bins=20)
 plt.show()
 
-#量的変数と順序変数の相関
+    #量的変数と順序変数の相関
 grid=sns.FacetGrid(train_df,col='Survived',row='Pclass',height=2.2,aspect=1.6)
 grid.map(plt.hist,'Age',alpha=.5,bins=20)
 grid.add_legend()
 plt.show()
 
-#カテゴリー変数同士の相関
+    #カテゴリー変数同士の相関
 grid=sns.FacetGrid(train_df,row='Embarked',height=2.2,aspect=1.6)
 grid.map(sns.pointplot,'Pclass','Survived','Sex',palette='deep')
 grid.add_legend()
 plt.show()
-#カテゴリー変数と量的変数の相関
+
+    #カテゴリー変数と量的変数の相関
 grid=sns.FacetGrid(train_df,row='Embarked',col='Survived',height=2.2,aspect=1.6)
 grid.map(sns.barplot,'Sex','Fare',alpha=0.5,ci=None)
 grid.add_legend()
 plt.show()
 
-
 #データ前処理
 
-
-#特徴量の削除
+    #特徴量の削除
 
 print("Before",train_df.shape,test_df.shape,combine[0].shape,combine[1].shape)
 
@@ -113,13 +109,14 @@ combine=[train_df,test_df]
 print("Adter",train_df.shape,test_df.shape,combine[0].shape,combine[1].shape)
 
 
-#新たな特徴量の作成
+    #新たな特徴量の作成
 
+    #敬称を抜き出す
 for dataset in combine:
-    dataset['Title']=dataset.Name.str.extract('([A-Za-z]+)¥.',expand=False)
+    dataset['Title']=dataset.Name.str.extract('([A-Za-z]+)\.',expand=False)
 
 print(pd.crosstab(train_df['Title'],train_df['Sex']))
-
+    #敬称を加工
 for dataset in combine:
     dataset['Title'] = dataset['Title'].replace(['Lady', 'Countess', 'Capt', 'Col', 'Don', 'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer', 'Dona'], 'Rare')
     dataset['Title'] = dataset['Title'].replace('Mlle', 'Miss')
@@ -128,12 +125,245 @@ for dataset in combine:
 
 print(train_df[['Title', 'Survived']].groupby(['Title'], as_index=False).mean())
 
-
+    #置換パターン
 title_mapping={"Mr":1,"Miss":2,"Mrs":3,"Master":4,"Rare":5}
 
+    #置換パターンに適応
 for dataset in combine:
     dataset['Title']=dataset['Title'].map(title_mapping)
     dataset['Title']=dataset['Title'].fillna(0)
 
 print(train_df.head())
+
+train_df=train_df.drop(['Name','PassengerId'],axis=1)
+test_df=test_df.drop(['Name'],axis=1)
+combine=[train_df,test_df]
+print(train_df.shape,test_df.shape)
+
+    #カテゴリー変数の変換
+
+for dataset in combine:
+    dataset['Sex']=dataset['Sex'].map({'female':1,'male':0}).astype(int)
+
+print(train_df.head())
+
+
+
+
+#可視化
+grid=sns.FacetGrid(train_df,row='Pclass',col='Sex',height=2.2,aspect=1.6)
+grid.map(plt.hist,'Age',alpha=0.5,bins=20)
+grid.add_legend()
+plt.show()
+
+guess_ages=np.zeros((2,3))
+print(guess_ages)
+
+#計算
+for dataset in combine:
+    for i in range(0, 2):
+        for j in range(0, 3):
+            guess_df = dataset[(dataset['Sex'] == i) & (dataset['Pclass'] == j + 1)]['Age'].dropna()
+
+# age_mean = guess_df.mean()
+# age_std = guess_df.std()
+# age_guess = rnd.uniform(age_mean - age_std, age_mean + age_std)
+            age_guess = guess_df.median()
+# Convert random age float to nearest .5 age
+            guess_ages[i, j] = int(age_guess / 0.5 + 0.5) * 0.5
+    for i in range(0, 2):
+        for j in range(0, 3):
+            dataset.loc[(dataset.Age.isnull()) & (dataset.Sex == i) & (dataset.Pclass == j + 1), 'Age'] = guess_ages[i, j]
+    dataset['Age'] = dataset['Age'].astype(int)
+    train_df.head()
+
+    #ageを順序変数に変換
+
+for dataset in combine:
+    dataset.loc[dataset['Age']<=16,'Age']=0
+    dataset.loc[(dataset['Age']>16)&(dataset['Age']<=32),'Age']=1
+    dataset.loc[(dataset['Age']>32)&(dataset['Age'] <=48), 'Age'] = 2
+    dataset.loc[(dataset['Age']>48) & (dataset['Age'] <=64), 'Age'] = 3
+    dataset.loc[dataset['Age']>64,'Age']
+print(train_df.head())
+
+    #既存の特徴を使って、新たな特徴量を作成
+    # FamilySizeという特徴量を作成する　parch&SibSP
+
+for dataset in combine:
+    dataset['FamilySize']=dataset['SibSp']+dataset['Parch']+1
+
+train_df[['FamilySize','Survived']].groupby(['FamilySize'],as_index=False).mean().sort_values(by='Survived',ascending=False)
+
+print(train_df.head())
+
+    # IsAlone
+
+for dataset in combine:
+    dataset['IsAlone']=0
+    dataset.loc[dataset['FamilySize']==1,'IsAlone']=1
+
+train_df[['IsAlone','Survived']].groupby(['IsAlone'],as_index=False).mean()
+
+print(train_df.head())
+
+    #カテゴリー変数の欠損値処理
+
+freq_port=train_df.Embarked.dropna().mode()[0]
+print(freq_port)
+
+for dataset in combine:
+    dataset['Embarked']=dataset['Embarked'].fillna(freq_port)
+
+
+train_df[['Embarked','Survived']].groupby(['Embarked'],as_index=False).mean().sort_values(by='Survived',ascending=False)
+
+print(train_df.head())
+
+    #カテゴリー変数の量的変換
+for dataset in combine:
+    dataset['Embarked']=dataset['Embarked'].map({'S':0,'C':1,'Q':2}).astype(int)
+
+print(train_df.head())
+
+    #Fareを前処理
+test_df['Fare'].fillna(test_df['Fare'].dropna().median(),inplace=True)
+print(train_df.info())
+
+#
+
+train_df['FareBand'] = pd.qcut(train_df['Fare'], 4)
+train_df[['FareBand', 'Survived']].groupby(['FareBand'], as_index=False).mean().sort_values(by='FareBand', ascending=True)
+
+for dataset in combine:
+    dataset.loc[dataset['Fare']<=7.91,'Fare']=0
+    dataset.loc[(dataset['Fare']>7.91)&(dataset['Fare']<=14.454),'Fare']=1
+    dataset.loc[(dataset['Fare']>14.454)&(dataset['Fare']<=31),'Fare']=2
+    dataset.loc[dataset['Fare'] >31,'Fare'] = 3
+    dataset['Fare']=dataset['Fare'].astype(int)
+
+
+train_df=train_df.drop(['FareBand'],axis=1)
+combine=[train_df,test_df]
+
+print(train_df.head(10))
+
+
+#モデル作成
+
+X_train=train_df.drop("Survived",axis=1)
+Y_train=train_df["Survived"]
+
+X_test=test_df.drop("PassengerId",axis=1).copy()
+print(X_train.shape,Y_train.shape,X_test.shape)
+
+
+
+    #logistic Regression
+
+print("LogisticRegression")
+logreg=LogisticRegression()
+logreg.fit(X_train,Y_train)
+Y_pred=logreg.predict(X_test)
+acc_log=round(logreg.score(X_train,Y_train)*100,2)
+print(acc_log)
+
+    #決定係数確認
+coeff_df=pd.DataFrame(train_df.columns.delete(0))
+coeff_df.columns=['Feature']
+coeff_df["Correlation"]=pd.Series(logreg.coef_[0])
+
+print(coeff_df.sort_values(by="Correlation",ascending=False))
+
+    #support vector machine
+print("support vector machine")
+svc=SVC()
+svc.fit(X_train,Y_train)
+Y_pred=svc.predict(X_test)
+acc_svc=round(svc.score(X_train,Y_train)*100,2)
+print(acc_svc)
+
+    #KNeigbhorClassifier
+print("KNeigbhorClassifier")
+knn=KNeighborsClassifier(n_neighbors=3)
+knn.fit(X_train,Y_train)
+Y_pred=knn.predict(X_test)
+acc_knn=round(knn.score(X_train,Y_train)*100,2)
+print(acc_knn)
+
+    #Gaussian Naive Bayes
+print("Gaussian Naive Bayes")
+gaussian=GaussianNB()
+gaussian.fit(X_train,Y_train)
+Y_pred=gaussian.predict(X_test)
+acc_gaussian=round(gaussian.score(X_train,Y_train)*100,2)
+print(acc_gaussian)
+
+    #Perceptron
+print("Perception")
+perceptron=Perceptron()
+perceptron.fit(X_train,Y_train)
+Y_pred=perceptron.predict(X_test)
+acc_perceptron=round(perceptron.score(X_train,Y_train)*100,2)
+print(acc_perceptron)
+
+
+    #Linear SVC
+
+linear_svc=LinearSVC()
+linear_svc.fit(X_train,Y_train)
+Y_pred=linear_svc.predict(X_test)
+acc_linear_svc=round(linear_svc.score(X_train,Y_train)*100,2)
+
+
+    #stochastic gradient descent
+print("SGDClassifier")
+sgd=SGDClassifier()
+sgd.fit(X_train,Y_train)
+Y_pred=sgd.predict(X_test)
+acc_sgd=round(sgd.score(X_train,Y_train)*100,2)
+print(acc_sgd)
+
+
+    #decision Tree
+decision_tree=DecisionTreeClassifier()
+decision_tree.fit(X_train,Y_train)
+Y_pred=decision_tree.predict(X_test)
+acc_decision_tree=round(decision_tree.score(X_train,Y_train)*100,2)
+print(acc_decision_tree)
+
+
+    #random forest
+random_forest=RandomForestClassifier(n_estimators=100)
+random_forest.fit(X_train,Y_train)
+Y_pred=random_forest.predict(X_test)
+random_forest.score(X_train,Y_train)
+acc_random_forest=round(random_forest.score(X_train,Y_train)*100,2)
+print(acc_random_forest)
+
+
+#モデル評価
+
+models = pd.DataFrame({
+    'Model': ['Support Vector Machines', 'KNN', 'Logistic Regression',
+              'Random Forest', 'Naive Bayes', 'Perceptron',
+              'Stochastic Gradient Decent', 'Linear SVC',
+              'Decision Tree'],
+    'Score': [acc_svc, acc_knn, acc_log,
+              acc_random_forest, acc_gaussian, acc_perceptron,
+              acc_sgd, acc_linear_svc, acc_decision_tree]})
+models=models.sort_values(by='Score', ascending=False)
+
+submission = pd.DataFrame({
+        "PassengerId": test_df["PassengerId"],
+        "Survived": Y_pred
+    })
+
+print("this is score",models)
+
+
+#リンク
+
+
+
 
